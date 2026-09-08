2e3f29d07cb57bbea6ef0fe73664dcf4a9b8fb70
haminahannanarpita <hamina172002@gmail.com>
2026-09-09 00:55:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import Countries from './componentes/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+      <section id="center">
+        
+        <div>
+          <h1>react world on the go</h1>
+          <Countries />
+          
+        </div>
+       
+      </section>
+
+    
+    </>
+  )
+}
+
+export default App
